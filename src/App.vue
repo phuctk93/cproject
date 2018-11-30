327505e8b70f5f0d956f0a1047845e4eaa8c0594
Bui Duc Phuc <phuctk93@debian.picostudio.xyz>
2018-12-01 01:42:49 +0700

    add simulator button

--- src/App.vue
+++ src/App.vue
@@ -27,15 +27,14 @@
     <v-toolbar color="indigo" dark fixed app>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn>USER PROFILE</v-btn>
     </v-toolbar>
     <v-content>
       <v-container grid-list-md>
         <router-view />
       </v-container>
     </v-content>
-    <v-footer color="indigo" app>
-      <span class="white--text">&copy; 2017</span>
-    </v-footer>
   </v-app>
 </template>
 
